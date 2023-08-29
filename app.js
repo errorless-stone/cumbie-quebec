9ff59aea48e8d791f7ed8bf22144761006602a69
Garner <jgarner6@una.edu>
2023-08-29 13:44:06 -0500

    Quarter of the way done with November

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require('express')
 const app = express()
+const port = (process.env.PORT || 3000)
+
+app.set('view engine', 'ejs')
+
+let myVariableServer = 'soft coded server data';
+
+app.get('/view', function (req, res) {
+  res.render('index',
+  {
+    'myVariableClient' : myVariableServer
+  }
+  );
+})
 
 app.get('/', function (req, res) {
   res.send('Hello World from Express & a PaaS/Render')
 })
 
-app.listen(3000)
+app.get('/index', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+
+
+//app.listen(3000)
+
+app.listen(port, () => console.log(`Server is running...on ${ port }` ));
