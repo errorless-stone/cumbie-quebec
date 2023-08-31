38aedb251f19b52a5462771f812dc527ad611d71
Garner <jgarner6@una.edu>
2023-08-31 13:44:44 -0500

    Day 3

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = (process.env.PORT || 3000)
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
+4
 let myVariableServer = 'soft coded server data';
 
 app.get('/view', function (req, res) {
-  res.render('index',
+  res.render('index', 
   {
-    'myVariableClient' : myVariableServer
+    'myVariableClient' : myVariableServer 
   }
   );
 })
 
+app.post('/postClientData', function (req, res) {
+  
+   console.log("body: ", req.body)
+   console.log("user Name: ", req.body.userName)
+  //  console.log("params: ", req.params['userName']);
+  
+  // myVariableServer = req.body.userName;
+
+  res.render('index', 
+  {
+    'myVariableClient' : req.body.userName 
+  }
+  );
+})
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World from Express & a PaaS/Render')
+  res.send('<h1>Hello World From Express & a PaaS/Render</h1>')
 })
 
 app.get('/index', function (req, res) {
@@ -24,6 +43,6 @@ app.get('/index', function (req, res) {
 
 
 
-//app.listen(3000)
+// app.listen(3000)
 
 app.listen(port, () => console.log(`Server is running...on ${ port }` ));
